0543a8a1547cdf9476c67f7df406a941239db591
Braden <fish2355@hotmail.com>
2019-12-11 09:57:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  let [nasa, setNasa] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=RR4nfp6N4yPogjd0s32Q2GywGcMGDdxdhcRA86R0"
+      )
+      .then(res => {
+        setNasa(res.data);
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>{nasa.date}</h2>
       </header>
     </div>
   );
